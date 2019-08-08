cd20b49752501391893f255f0197d6f75b08df2c
Hamilton Perez <hambone124@users.noreply.github.com>
2019-08-08 15:17:13 -0400

    Added debounce to reduce repeated keystrokes

--- keyboards/ergodox_ez/keymaps/hambone124/config.h
+++ keyboards/ergodox_ez/keymaps/hambone124/config.h
@@ -2,6 +2,9 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#undef DEBOUNCE
+#define DEBOUNCE 30
+
 #undef RGBLIGHT_HUE_STEP
 #define RGBLIGHT_HUE_STEP 6
 
